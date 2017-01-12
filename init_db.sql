ec36f77cb8d25caff972a96cc90dd2583da42bb9
phtaran <differenceN2@yandex.ru>
2017-01-12 16:45:39 +0200

    Add sql script to init database

---
+++ init_db.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE person
+(
+  id integer NOT NULL DEFAULT nextval('serial'::regclass),
+  age integer,
+  content character varying(255),
+  name character varying(255),
+  CONSTRAINT person_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE person
+  OWNER TO postgres;
